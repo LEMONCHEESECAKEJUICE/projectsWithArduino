00733e5a80ab1c5f3baab68db150854ccd2c2099
LEMONCHEESECAKEJUICE <lemon.cheese.cake.juice@gmail.com>
2016-12-24 02:45:45 +0900

    Add files via upload

---
+++ blinkSOS.ino
@@ -0,0 +1,23 @@
+const int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  for (int i=0; i<3; i++) {
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(300);
+  }
+
+  for (int i=0; i<3; i++) {
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(300);
+  }
+  
+}
+
